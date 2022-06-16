a91fca9be6702fd058942ae940da47bad0917608
EduardoDiaz <adch.edch@gmail.com>
2022-06-16 17:41:02 -0400

    fixing de texto encapsulado

--- components/Resumen/BlockSobreMiEvento.tsx
+++ components/Resumen/BlockSobreMiEvento.tsx
@@ -275,7 +275,7 @@ interface propsElement extends schemaItem {
 
 const AboutItem: FC<propsElement> = ({ title, value, toggleClick }) => {
 
-  const textoRecortado = value.title.substring(0,10)+"..."
+  
 
   return (
     <>
@@ -295,7 +295,7 @@ const AboutItem: FC<propsElement> = ({ title, value, toggleClick }) => {
             {title && capitalize(title)}
           </p>
           <p className={'font-display font-base text-xs md:text-sm text-gray-700 font-semibold'}>
-            {value?.title && value.title? textoRecortado: value.title}
+            {value?.title  ? value.title.substring(0,10)+"...": value.title}
           </p>
         </span>
       </button>
