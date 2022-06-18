c0a730737d7d82725961a0d95c8eb7b0b28c078a
Jafet <febmerlib@gmail.com>
2022-06-18 00:08:16 +0000

    removiodo nombre

--- components/DefaultLayout/Breadcumb.tsx
+++ components/DefaultLayout/Breadcumb.tsx
@@ -1,15 +1,13 @@
 import Link from 'next/link'
 import React from 'react'
 import { FlechaIcon } from '../icons'
-import {EventContextProvider} from "../../context";
 
 const Breadcumbs = () => {
-    const {event} = EventContextProvider()
     return (
         <div className="flex gap-2 items-center w-full py-2 font-display text-sm text-gray-500 cursor-pointer hover:text-gray-400  transform transition">
             <FlechaIcon />
             <Link href="/resumen-evento" passHref>
-            <p >Volver a resumen del evento: {event?.nombre}</p>
+                <p >Volver a resumen del evento: </p>
             </Link>
         </div>
     )
