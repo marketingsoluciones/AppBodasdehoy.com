99ac322fa21f16a923133092a2df36d7ed85f4a5
Jafet <febmerlib@gmail.com>
2022-10-01 20:06:54 +0000

    fix profile in mobile

--- components/DefaultLayout/NavigationMobile.js
+++ components/DefaultLayout/NavigationMobile.js
@@ -1,15 +1,33 @@
-import { useEffect, useState } from "react";
-import { AuthContextProvider, EventContextProvider } from "../../context";
+import { useEffect, useRef, useState } from "react";
+import { AuthContextProvider } from "../../context";
 import Link from "next/link";
 import { InvitacionesIcon, InvitadosIcon, MesasIcon, MisEventosIcon } from "../icons";
-import ClickAwayListener from "react-click-away-listener";
 import router from "next/router";
 
+const useOutsideSetShow = (ref, setShow) => {
+  const handleClickOutside = (event) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setTimeout(() => {
+        setShow(false)
+      }, 50);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  });
+};
 
 const NavigationMobile = () => {
+  const wrapperRef = useRef(null);
   const { user } = AuthContextProvider();
   const [show, setShow] = useState(false)
-  console.log(show)
+  useEffect(() => {
+    console.log(show)
+  }, [show])
 
   const Navbar = [
     { title: "Mis eventos", icon: <MisEventosIcon className="text-primary w-7 h-7" />, route: "/" },
@@ -17,7 +35,7 @@ const NavigationMobile = () => {
     { title: "Invitaciones", icon: <InvitacionesIcon className="text-primary w-7 h-7" />, route: "/invitaciones" },
     { title: "Mesas", icon: <MesasIcon className="text-primary w-7 h-7" />, route: "/mesas" },
   ]
-
+  useOutsideSetShow(wrapperRef, setShow);
   return (
     <>
       <ul className="f-bottom md:hidden bg-white z-50 rounded-t-2xl h-max py-5 shadow-lg w-full fixed bottom-0 grid grid-cols-5 place-items-center">
@@ -30,17 +48,20 @@ const NavigationMobile = () => {
           </Link>
         ))}
 
-
-        <li onClick={() => setShow(!show)} className="text-blue-primary hover:text-blue-secondary cursor-pointer transition" >
-          <img  src={user?.photoURL ?? "/placeholder/user.png"} className="w-10 h-10 rounded-full" />
-        </li>
+        <div className="w-10 h-10 truncate">
+          <li onClick={() => {
+            setShow(!show)
+          }} className="text-blue-primary hover:text-blue-secondary cursor-pointer transition" >
+            <img src={user?.photoURL ?? "/placeholder/user.png"} className="w-10 h-10 rounded-full" />
+          </li>
+          {show &&
+            <div ref={wrapperRef} >
+              <ProfileMenu />
+            </div>
+          }
+        </div>
 
       </ul>
-      <ClickAwayListener onClickAway={() => show && setShow(true)}>
-        <span className={`${show ? "  block absolute right-5 bottom-24 md:right-56" : "hidden"}`}>
-          <ProfileMenu />
-        </span>
-      </ClickAwayListener>
     </>
   );
 };
@@ -49,7 +70,7 @@ const ProfileMenu = () => {
   const { user } = AuthContextProvider();
   return (
     <>
-      <div className={`w-40 h-16 rounded-md h-max  bg-white shadow-md  right-2 inset-y-full overflow-hidden z-100 `}>
+      <div className={`bg-white w-40 h-16 rounded-md shadow-md overflow-hidden absolute transform translate-x-[calc(-122px)] translate-y-[calc(-110px)]`}>
         <ul className="w-full">
 
           {!user && <li className="w-full pl-5 py-1 text-gray-500 transition  hover:bg-primary hover:text-white font-display text-sm">
@@ -59,8 +80,8 @@ const ProfileMenu = () => {
           <Link href={process.env.NEXT_PUBLIC_DIRECTORY} passHref>
             <li
               className="w-full pl-5 py-1 text-gray-500 transition  hover:bg-primary hover:text-white font-display text-sm"
-            > 
-              <p>ir al directorio</p>
+            >
+              <p>Ir al directorio</p>
             </li>
           </Link>
 
