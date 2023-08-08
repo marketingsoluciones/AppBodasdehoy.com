64f8230d8215c15a1a8e82386657e44932e89e8f
eduardo andres <adch.edch@gmail.com>
2023-08-08 15:44:48 -0400

    BODAPP-658 changed toltip string

--- components/Mesas/EditMesa.tsx
+++ components/Mesas/EditMesa.tsx
@@ -2,7 +2,7 @@ import { FC } from "react"
 import { EventContextProvider } from "../../context"
 import { fetchApiEventos, queries } from "../../utils/Fetching"
 import { table } from "../../utils/Interfaces"
-import { BorrarIcon, EditarIcon } from "../icons"
+import { BorrarIcon, EditarIcon, Lock } from "../icons"
 import { useToast } from '../../hooks/useToast';
 
 
@@ -12,7 +12,7 @@ interface propsEditMesa {
   disableDrag: any
 }
 
-export const EditMesa: FC<propsEditMesa> = ({ mesa, setShowFormEditar ,disableDrag }) => {
+export const EditMesa: FC<propsEditMesa> = ({ mesa, setShowFormEditar, disableDrag }) => {
   const toast = useToast()
   const { event, setEvent } = EventContextProvider()
   const handleDeleteMesa = async () => {
@@ -25,10 +25,10 @@ export const EditMesa: FC<propsEditMesa> = ({ mesa, setShowFormEditar ,disableDr
     })
     setEvent((old) => ({ ...old, mesas_array }))
   }
-  
-  
 
-  
+
+
+
   return (
     <div className="w-[20px] absolute transform -translate-x-[30px] -translate-y-[15px] block">
       <button onClick={() => { handleDeleteMesa() }}>
@@ -37,10 +37,8 @@ export const EditMesa: FC<propsEditMesa> = ({ mesa, setShowFormEditar ,disableDr
       <button onClick={() => { setShowFormEditar({ mesa: mesa, visible: true }) }}>
         <EditarIcon className="text-gray-600 w-4 h-4" />
       </button>
-      <button className={`${disableDrag?"block":"hidden"}  `} onClick={()=>{toast("error","Desbloquea las mesas para poder reubicarlas ")}}>
-      {/* <span className="animate-ping absolute inline-flex h-1.5 w-1.5 rounded-full bg-sky-400 opacity-75"></span>
-      <span className="absolute inline-flex rounded-full h-1.5 w-1.5 bg-sky-500"></span> */}
-        <img src="advertencia.png" alt="alert"  className="text-gray-600 w-4 h-4 "/>
+      <button className={`${disableDrag ? "block" : "hidden"}   `} onClick={() => { toast("error", "Desbloquea el plano para poder mover las mesas ") }}>
+        <Lock className="hidden* md:block" />
       </button>
     </div >
   )
