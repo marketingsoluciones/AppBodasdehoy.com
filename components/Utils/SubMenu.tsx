623dd3f204a9a6180caed2711cc67304348955de
Jafet <febmerlib@gmail.com>
2023-09-06 20:57:46 +0200

    added icons subMenu mesas

--- components/Utils/SubMenu.tsx
+++ components/Utils/SubMenu.tsx
@@ -1,22 +1,33 @@
-import { MesaIcon } from "../icons"
+import { InvitadosIcon, MesaIcon } from "../icons"
+import { GiGrandPiano } from 'react-icons/gi';
+import { BsIntersect } from 'react-icons/bs';
+import { ImInsertTemplate } from 'react-icons/im';
+import { HiDocumentReport } from 'react-icons/hi';
+
 const sutMenus = [
   {
     title: "invitados",
+    icon: <InvitadosIcon className="w-6 h-6" />,
   },
   {
     title: "mesas",
+    icon: <MesaIcon className="w-6 h-6" />,
   },
   {
     title: "mobiliario",
+    icon: <GiGrandPiano className="w-6 h-6" />,
   },
   {
     title: "zonas",
+    icon: <BsIntersect className="w-6 h-6" />,
   },
   {
     title: "plantilla",
+    icon: <ImInsertTemplate className="w-6 h-6" />,
   },
   {
     title: "resumen",
+    icon: <HiDocumentReport className="w-6 h-6" />,
   },
 ]
 
@@ -29,11 +40,11 @@ export const SubMenu = ({ itemSelect, setItemSelect }) => {
   }
 
   return (
-    <div className="w-full h-full gap-2 px-2 py-1 flex">
+    <div className="w-full h-full *gap-1 px-[2px] py-[1px] flex">
       {sutMenus.map((elem: any, idx: number) => {
         return (
-          <div key={idx} onClick={() => handleClick(elem)} className={`w-1/6 h-full flex flex-col items-center justify-center rounded-sm ${elem.title == itemSelect ? "bg-base text-primary font-semibold" : "bg-primary text-white"} ${elem?.title == "invitados" && "md:hidden"}`}>
-            <MesaIcon className="w-6 h-6" />
+          <div key={idx} onClick={() => handleClick(elem)} className={`w-1/6 h-full flex flex-col items-center justify-center rounded-lg ${elem.title == itemSelect ? "bg-base text-primary font-semibold" : "bg-primary text-white"} ${elem?.title == "invitados" && "md:hidden"}`}>
+            {elem?.icon}
             <span className={`capitalize text-[10px] leading-none`}>{elem?.title}</span>
           </div>
         )
