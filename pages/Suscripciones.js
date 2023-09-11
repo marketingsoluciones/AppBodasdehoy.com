3adcc6ce912ac8bd9249deaab5a8d4c0dfb587f7
eduardo andres <adch.edch@gmail.com>
2023-09-11 11:51:08 -0400

    chages on suscription page

--- pages/Suscripciones.js
+++ pages/Suscripciones.js
@@ -5,17 +5,14 @@ const Suscripciones = () => {
 
   return (
     <>
-      <div className="  py-20 ">
+      <div className="  py-5 bg-base  ">
+        <div className="flex justify-center pb-10 font-body text-3xl text-center text-primary ">
+          <p> Optimiza tus eventos con acceso completo <br /> a herramientas y servicios</p>
+        </div>
         {
-          true ? <div className="">
+          <div className="">
             <Script async src="https://js.stripe.com/v3/pricing-table.js"></Script>
-            <stripe-pricing-table pricing-table-id="prctbl_1No7YEIUSdlgn0XNDLV0kNgK"
-              publishable-key="pk_test_nyHp6UvsADBtFr6LUquKdgPP">
-            </stripe-pricing-table>
-
-          </div> : <div>
-            <Script async src="https://js.stripe.com/v3/pricing-table.js"></Script>
-            <stripe-pricing-table pricing-table-id="prctbl_1No7gNIUSdlgn0XNG1io0Uyx"
+            <stripe-pricing-table pricing-table-id="prctbl_1NpC5HIUSdlgn0XN3DUgc0ce"
               publishable-key="pk_test_nyHp6UvsADBtFr6LUquKdgPP">
             </stripe-pricing-table>
           </div>
