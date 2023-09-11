0a22558140d93171bbae906764075f6422a26d13
Jafet <febmerlib@gmail.com>
2023-09-11 09:14:31 +0200

    fixed

--- components/Mesas/BlockPanelMesas.tsx
+++ components/Mesas/BlockPanelMesas.tsx
@@ -26,7 +26,7 @@ const BlockPanelMesas: FC<propsBlockPanelMesas> = ({ set, state, setModelo }) =>
     <div id="listTables" className="js-drop-mesas bg-white w-full h-full">
       <BlockDefault>
         {ListaMesas.map((item, idx) => (
-          <DragTable item={item} set={set} state={state} setModelo={setModelo} />
+          <DragTable key={idx} item={item} set={set} state={state} setModelo={setModelo} />
           // <div onClick={() => handleClick(item.title)} key={idx} className="w-full h-full p-2 flex-col justify-center items-center cursor-pointer">
           //   <div key={idx} className="jkrelative w-full h-full flex transform hover:scale-105 transition justify-center items-center">
           //     {item.icon}
