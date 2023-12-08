e245764a3765580f1d681dc0b312d5a0edf8ae0b
Jafet <febmerlib@gmail.com>
2023-12-08 20:24:30 +0100

    fixed error deployed

--- components/Forms/InputField.tsx
+++ components/Forms/InputField.tsx
@@ -1,8 +1,8 @@
 import { useField } from "formik"
 import React, { FC, InputHTMLAttributes, useEffect, useState } from "react"
 import { WarningIcon } from '../icons'
-import { PhoneInput, usePhoneInput } from 'react-international-phone';
-import 'react-international-phone/style.css';
+// import { PhoneInput, usePhoneInput } from 'react-international-phone';
+// import 'react-international-phone/style.css';
 import { AuthContextProvider } from "../../context";
 
 interface propsInputField extends InputHTMLAttributes<HTMLInputElement> {
@@ -12,16 +12,16 @@ interface propsInputField extends InputHTMLAttributes<HTMLInputElement> {
 const InputField: FC<Partial<propsInputField>> = ({ label, className, ...props }) => {
   const { geoInfo } = AuthContextProvider()
   const [field, meta, helpers] = useField({ name: props.name })
-  useEffect(() => {
-    if (props?.type == "tel") {
-      (document.getElementsByClassName("react-international-phone-input")[0] as HTMLElement)
-        ?.focus()
-      const input = document.getElementsByClassName("react-international-phone-input")[0]
-      input?.setAttribute("class", `ml-2 font-display text-sm text-gray-500 border border-gray-100 focus:border-primary w-full py-2 px-4 rounded-xl focus:outline-none transition ${className}`)
-      // const select = document.getElementsByClassName("react-international-phone-country-selector-button")[0]
-      // select.setAttribute("class", `ml-2 font-display text-sm text-gray-500 border border-gray-100 focus:border-primary w-full py-2 px-4 rounded-xl focus:outline-none transition ${className}`)
-    }
-  }, [])
+  // useEffect(() => {
+  //   if (props?.type == "tel") {
+  //     (document.getElementsByClassName("react-international-phone-input")[0] as HTMLElement)
+  //       ?.focus()
+  //     const input = document.getElementsByClassName("react-international-phone-input")[0]
+  //     input?.setAttribute("class", `ml-2 font-display text-sm text-gray-500 border border-gray-100 focus:border-primary w-full py-2 px-4 rounded-xl focus:outline-none transition ${className}`)
+  //     // const select = document.getElementsByClassName("react-international-phone-country-selector-button")[0]
+  //     // select.setAttribute("class", `ml-2 font-display text-sm text-gray-500 border border-gray-100 focus:border-primary w-full py-2 px-4 rounded-xl focus:outline-none transition ${className}`)
+  //   }
+  // }, [])
 
   return (
     <div className="w-full h-max relative">
@@ -30,11 +30,11 @@ const InputField: FC<Partial<propsInputField>> = ({ label, className, ...props }
         {props?.type !== "tel"
           ? <input className={`font-display text-sm text-gray-500 border-[1px] border-gray-200 focus:border-gray-400 w-full py-2 px-4 rounded-xl focus:ring-0 focus:outline-none transition ${className}`} {...field} {...props}></input>
           : <div onBlur={() => helpers.setTouched(true)} >
-            <PhoneInput
+            {/* <PhoneInput
               defaultCountry={geoInfo?.ipcountry.toLowerCase()}
               value={field?.value}
               onChange={(value) => { helpers.setValue(value) }}
-            />
+            /> */}
           </div>
         }
       </div>
