2fd1ea7d2877e2aaa879a13ada3e82af4b4992cb
Jafet <febmerlib@gmail.com>
2023-12-27 18:11:56 +0100

    BODAPP-802

--- components/Utils/BlockTitle.js
+++ components/Utils/BlockTitle.js
@@ -9,21 +9,20 @@ const BlockTitle = ({ title }) => {
   return (
     <div className={`w-full h-14 bg-white rounded-xl shadow-lg ${forCms ? "hidden" : "flex"} items-center justify-between`}>
       <div className='flex flex-col px-6 font-display'>
-        <span className='capitalize text-gray-600 leading-[20px] font-medium'>{event?.nombre}</span>
-        <span className='capitalize text-primary text-[12px] leading-[12px]'>{event?.tipo}</span>
+        <span className='md:hidden capitalize text-gray-600 leading-[20px] font-medium'>{event?.nombre}</span>
+        <span className='md:hidden capitalize text-primary text-[12px] leading-[12px]'>{event?.tipo}</span>
         <span className="text-gray-500 text-[18px] leading-[20px] font-bold">
           {title}
         </span>
       </div>
       <div className='w-[23%] md:w-[35%] h-[100%] flex items-center '>
-
         <img
           src={defaultImagenes[event?.tipo]}
           className=" h-[90%] object-cover object-top rounded-md border-1 border-gray-600"
           alt={event?.nombre}
         />
-        <div className='hidden md:flex flex-col font-display font-semibold text-md text-gray-500 px-2'>
-          <span className='text-sm translate-y-2'>Evento</span>
+        <div className='hidden md:flex flex-col font-display font-semibold text-md text-gray-500 px-2 gap-2'>
+          <span className='text-sm translate-y-2 text-primary text-[12px] first-letter:capitalize'>{event?.tipo}</span>
           <span className='uppercase w-64 truncate '>{event?.nombre}</span>
         </div>
 
