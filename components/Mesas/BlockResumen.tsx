e7c659a57253733beca0884c5c0c7c95e89762a3
Jafet <febmerlib@gmail.com>
2023-12-27 23:53:54 +0100

    fixed resumen

--- components/Mesas/BlockResumen.tsx
+++ components/Mesas/BlockResumen.tsx
@@ -1,6 +1,6 @@
 import { FC, useEffect, useState } from "react"
 import { EventContextProvider } from "../../context"
-import { MesaIcon } from "../icons"
+import { InvitadosIcon, MesaIcon } from "../icons"
 import { guests } from '../../utils/Interfaces';
 
 interface propsBlockResumen {
@@ -19,21 +19,21 @@ const BlockResumen: FC<propsBlockResumen> = ({ InvitadoSentados }) => {
         { title: `${InvitadoSentados?.length} de ${event?.invitados_array?.length}`, subtitle: " Invitados sentados" },
     ]
     return (
-        <div className="bg-primary w-full pb-2 flex flex-col items-center justify-center rounded-lg h-full overflow-y-auto ">
+        <div className="bg-primary w-[calc(100%-16px)] h-[calc(100%-6px)] m-auto flex flex-col rounded-lg overflow-y-auto pt-2">
             {
                 event.planSpace.map((item, idx) => {
                     return (
-                        <div key={idx}>
-                            <h2 className="text-tertiary font-display text-medium md:py-1">{item?.title}</h2>
-                            <div className="flex space-x-2 items-center justify-center">
-                                <div className="flex w-max gap-3  items-center justify-center">
-                                    <MesaIcon className="text-white h-3 w-auto" />
+                        <div key={idx} className="md:mb-3 px-2">
+                            <h2 className="text-tertiary font-display text-medium md:text-lg capitalize -mb-1">{item?.title}</h2>
+                            <div className="flex flex-wrap items-center">
+                                <div className="flex w-28 items-center ml-2">
+                                    <MesaIcon className="text-white w-6 h-6" />
                                     <p className="text-white m-1 font-display *font-semibold *text-lg leading-4 text-[12px]">
                                         {item?.tables?.length} {/* <span className="text-xs md:text-sm m- font-light text-right"> */} mesas{/* </span> */}
                                     </p>
                                 </div>
-                                <div className="flex w-max gap-3  items-center justify-center">
-                                    <MesaIcon className="text-white h-3 w-auto" />
+                                <div className="flex w-max items-center">
+                                    <InvitadosIcon className="text-white w-6 h-6 ml-2 mr-1" />
                                     {(() => {
                                         if (item.tables.length != 0) {
                                             const invi = item.tables.map((item) => {
@@ -41,14 +41,14 @@ const BlockResumen: FC<propsBlockResumen> = ({ InvitadoSentados }) => {
                                             })
                                             const inviReduce = invi.flat()
                                             return (
-                                                < p key={idx} className="text-white m-1 leading-4 text-[12px]" >
+                                                < p key={idx} className="bg-red text-white m-1 leading-4 text-[12px]" >
                                                     {inviReduce.length} de {event?.invitados_array?.length}
                                                     {/* <span className="text-xs md:text-sm m- font-light text-right"> */} Invitados sentados{/* </span> */}
                                                 </p>
                                             )
                                         } else {
                                             return (
-                                                <p className="text-white m-1* font-display leading-4 text-[12px] *w-full ">
+                                                <p className="text-white font-display leading-4 text-[12px] ">
                                                     0 de {event?.invitados_array?.length} {/* <span className=" md:text-[10px] text-right"> */} Invitados sentados{/* </span> */}
                                                 </p>
                                             )
