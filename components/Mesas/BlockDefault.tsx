2f8f4cc12fecfd508d556f95220b50e5e20c9d2d
Jafet <febmerlib@gmail.com>
2023-12-27 19:32:01 +0100

    BODAPP-819 BODAPP-820

--- components/Mesas/BlockDefault.tsx
+++ components/Mesas/BlockDefault.tsx
@@ -12,12 +12,12 @@ const BlockDefault: FC<any> = ({ children, listaLength }) => {
   const handleResize = () => {
     let a = 0
     if (window.innerWidth < 1536) {
-      const r = Math.trunc(listaLength / 3 + 1)
-      if (r == 1) setClassNameGrid("h-[50%] grid-rows-1 grid-cols-3")
-      if (r == 2) setClassNameGrid("h-[100%] grid-rows-2 grid-cols-3")
-      if (r == 3) setClassNameGrid("h-[150%] grid-rows-3 grid-cols-3")
-      if (r == 4) setClassNameGrid("h-[200%] grid-rows-4 grid-cols-3")
-      if (r == 5) setClassNameGrid("h-[250%] grid-rows-5 grid-cols-3")
+      const r = Math.trunc(listaLength / 4 + 1)
+      if (r == 1) setClassNameGrid("h-[50%] grid-rows-1 grid-cols-4")
+      if (r == 2) setClassNameGrid("h-[100%] grid-rows-2 grid-cols-4")
+      if (r == 3) setClassNameGrid("h-[150%] grid-rows-3 grid-cols-4")
+      if (r == 4) setClassNameGrid("h-[200%] grid-rows-4 grid-cols-4")
+      if (r == 5) setClassNameGrid("h-[250%] grid-rows-5 grid-cols-4")
     }
     // if (window.innerWidth > 768 && window.innerWidth < 1536) {
     //   const r = Math.trunc(listaMesasLength / 3 + 1)
@@ -49,7 +49,7 @@ const BlockDefault: FC<any> = ({ children, listaLength }) => {
 
   return (
     <div className="w-full h-full overflow-auto text-sm">
-      <div className={`grid ${classNameGrid}`} >
+      <div className={`w-full truncate grid ${classNameGrid}`} >
         {children}
       </div>
     </div>
