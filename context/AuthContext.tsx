5d9c6dff0f1cad530b3995568cf75b9b6e6bb97b
Jafet <febmerlib@gmail.com>
2024-01-19 05:22:54 +0000

    working

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -8,6 +8,7 @@ import { fetchApiBodas, fetchApiEventos, queries } from "../utils/Fetching";
 import { boolean } from "yup";
 import { initializeApp } from "firebase/app";
 import { useRouter } from "next/router";
+import { parseJwt } from "../utils/Authentication";
 
 const initialContext = {
   user: undefined,
@@ -119,7 +120,11 @@ const AuthProvider = ({ children }) => {
 
         onAuthStateChanged(getAuth(), async (user) => {
           const sessionCookie = Cookies.get(config?.cookie);
-          console.info(8000042, "Verificando cookie", sessionCookie);
+          const asd = parseJwt(sessionCookie)
+          console.info(8000042, "Verificando cookie", user?.uid, asd?.user_id);
+          if (user?.uid !== asd?.user_id) {
+            sessionCookie && signInWithCustomToken(getAuth(), sessionCookie)
+          }
           //setUser(user)
           if (!sessionCookie) {
             const cookieContent = JSON.parse(Cookies.get(config?.cookieGuest) ?? "{}")
