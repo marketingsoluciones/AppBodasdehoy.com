8e0dc00ac83ae4032f3399d48ed7325f64204754
eduardo andres <adch.edch@gmail.com>
2024-05-21 14:47:33 -0400

    working

--- components/DefaultLayout/Container.tsx
+++ components/DefaultLayout/Container.tsx
@@ -14,21 +14,25 @@ const Container = (props) => {
   useEffect(() => {
     setLoading(false)
   }, [])
-  console.log("Container")
+
+  const urls = ["/info-app", "/confirmar-asistencia"]
+
   return (
     <>
-            {router?.pathname == "/RelacionesPublicas" ? null: <><NavigationMobile />
+      {router?.pathname == "/RelacionesPublicas" ? null : <>
+        <NavigationMobile />
         {!forCms && <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: { duration: 1, delay: 0.5 } }}
-        className="md:block relative"
-      >
-        <Navigation />
-      </motion.div>
-      }</>
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 1, delay: 0.5 } }}
+          className="md:block relative"
+        >
+          <Navigation />
+        </motion.div>
+        }
+      </>
       }
 
-      <div className={`*max-w-[70%] *max-h-[10%] w-[100%]  overflow-auto ${router?.pathname == "/info-app" ? "" : forCms ? "h-[100vh]" : "h-[calc(100vh-144px)]"}`}>
+      <div className={`*max-w-[70%] w-[100%]  overflow-auto ${urls.includes(router?.pathname) ? "" : forCms ? "h-[100vh]" : "h-[calc(100vh-144px)]"}`}>
         <main>
           {children}
         </main>
