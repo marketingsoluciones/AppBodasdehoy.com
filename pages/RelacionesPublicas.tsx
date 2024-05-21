a2febddc1093d75ff4fd391f3f74c1f62f80c3a2
ChristianWork05 <christian.lanza@recargaexpress.com>
2024-05-21 12:45:01 -0400

    BODAPP-880 NUEVO MODULO RRPP 1.1

---
+++ pages/RelacionesPublicas.tsx
@@ -0,0 +1,53 @@
+import { FC, useState } from "react";
+import CancelarReserva from "../components/RRPP/CancelarReserva";
+import EntradasGratis from "../components/RRPP/EntradasGratis";
+import PrincipalDE from "../components/RRPP/PrincipalDE";
+import ReciboEntradas from "../components/RRPP/ReciboEntradas";
+import RegistroEntradasUser from "../components/RRPP/RegistroEntradasUser";
+import ReservaCantidad from "../components/RRPP/ReservaCantidad";
+import ReservaDatos from "../components/RRPP/ReservaDatos";
+import VentasEntradas from "../components/RRPP/VentasEntradas";
+
+
+const RelacionesPublicas: FC = () => {
+  const [optionSelect, setOptionSelect] = useState(0)
+  const handleClickOption = (idx) => {
+    setOptionSelect(idx);
+  };
+
+  const dataComponents = [
+    {
+      component: <PrincipalDE componentState={optionSelect} setComponentState={setOptionSelect} />
+    },
+    {
+      component: <VentasEntradas componentState={optionSelect} setComponentState={setOptionSelect} />
+    },
+    {
+      component: <EntradasGratis componentState={optionSelect} setComponentState={setOptionSelect} />
+    },
+    {
+      component: <RegistroEntradasUser componentState={optionSelect} setComponentState={setOptionSelect} />
+    },
+    {
+      component: <ReciboEntradas componentState={optionSelect} setComponentState={setOptionSelect} />
+    },
+    {
+      component: <ReservaCantidad componentState={optionSelect} setComponentState={setOptionSelect} />
+    },
+    {
+      component: <ReservaDatos componentState={optionSelect} setComponentState={setOptionSelect} />
+    },
+    {
+      component: <CancelarReserva componentState={optionSelect} setComponentState={setOptionSelect} />
+    },
+  ]
+
+  return (
+    <div className="w-[100%] h-[100%] items-center justify-center">
+      <div id="rootElement" />
+      {dataComponents[optionSelect].component}
+    </div>
+  );
+};
+
+export default RelacionesPublicas;
