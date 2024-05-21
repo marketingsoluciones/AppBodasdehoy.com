34ce51e14a7487086655f0e286dff7a39b8708a7
ChristianWork05 <christian.lanza@recargaexpress.com>
2024-05-21 13:45:53 -0400

    BODAPP-880 modulo nuevo RRPP 1.2

--- components/DefaultLayout/Container.tsx
+++ components/DefaultLayout/Container.tsx
@@ -17,15 +17,17 @@ const Container = (props) => {
   console.log("Container")
   return (
     <>
-      <NavigationMobile />
-      {!forCms && <motion.div
+            {router?.pathname == "/RelacionesPublicas" ? null: <><NavigationMobile />
+        {!forCms && <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1, transition: { duration: 1, delay: 0.5 } }}
         className="md:block relative"
       >
         <Navigation />
       </motion.div>
+      }</>
       }
+
       <div className={`*max-w-[70%] *max-h-[10%] w-[100%]  overflow-auto ${router?.pathname == "/info-app" ? "" : forCms ? "h-[100vh]" : "h-[calc(100vh-144px)]"}`}>
         <main>
           {children}
