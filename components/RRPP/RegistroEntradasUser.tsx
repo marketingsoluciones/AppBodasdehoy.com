b779e348520b7bc7f02adae4f45a9d0801b35dfa
eduardo andres <adch.edch@gmail.com>
2024-06-03 14:07:52 -0400

    working

--- components/RRPP/RegistroEntradasUser.tsx
+++ components/RRPP/RegistroEntradasUser.tsx
@@ -20,15 +20,10 @@ const RegistroEntradasUser: FC<propsRegistroEntradasUser> = ({ componentState, s
   const initialCount = router?.query?.count
   const newCount = initialCount && +initialCount
   const datafilter = data?.data?.filter(element => (element.metadata.grupo === "ticket"))
-
   const FindTicket = datafilter?.find(({ name }) => name === ticket)
   const price = FindTicket?.prices[0]?.unit_amount / 100
   const TotalCompra = (count * price) + 8.25
-
   const [valuesForm, setValuesForm] = useState()
-
-  console.log("values", valuesForm)
-
   const handleSubmit = (values) => {
     console.log('Valores del formulario:', values)
   };
@@ -37,10 +32,10 @@ const RegistroEntradasUser: FC<propsRegistroEntradasUser> = ({ componentState, s
     <div className="bg-slate-100 w-full h-[100vh] flex flex-col gap-4 items-center justify-start  pt-[20px]">
       <HeaderComp componentState={componentState} setComponentState={setComponentState} />
       <div className="w-[100%] h-full text-left text-sm font-medium">
-        <CabeceraR componentState={componentState} setComponentState={setComponentState} />
+        <CabeceraR componentState={componentState} setComponentState={setComponentState} activo={false} />
         <div className="w-full flex md:flex-row flex-col items-start justify-center box-border gap-[25px] text-sm text-black">
           <div className="md:self-stretch  flex flex-col items-center justify-start gap-[21px] overflow-y-auto overflow-x-hidden h-[calc(100vh-210px)] pr-2 ">
-            {(() => {
+            {/* {(() => {
               const arr = [];
               for (let i = 0; i < newCount; i++) {
                 arr.push(
@@ -50,8 +45,8 @@ const RegistroEntradasUser: FC<propsRegistroEntradasUser> = ({ componentState, s
                 );
               }
               return arr;
-            })()}
-            {/* <DatosComprador handleSubmit={handleSubmit} idx={count} /> */}
+            })()} */}
+            <DatosComprador />
             <CheckCondition />
           </div>
           <ResumenComponents componentState={componentState} setComponentState={setComponentState} FindTicket={FindTicket} TotalCompra={TotalCompra} count={newCount} />
--- components/RRPP/Sub-Componentes/DatosComprador.tsx
+++ components/RRPP/Sub-Componentes/DatosComprador.tsx
@@ -1,28 +1,74 @@
-import { Formik, Form, Field, useFormikContext } from 'formik';
-import { FC, useEffect, useState } from "react"
-import { Correo } from "../../icons";
+import { Formik, Form } from 'formik';
+import { FC } from "react"
 import InputField from "../../Forms/InputField";
+import { useRouter } from 'next/router';
 
 interface propsDatosComprador {
-  idx: any;
+  idx?: any;
   handleSubmit?: any;
-  setValuesForm?:any;
-  valuesForm?:any
+  setValuesForm?: any;
+  valuesForm?: any;
+  count?: any;
 }
-export const DatosComprador: FC<propsDatosComprador> = ({ idx, handleSubmit, setValuesForm,valuesForm }) => {
+export const DatosComprador: FC<propsDatosComprador> = ({ }) => {
+  const router = useRouter()
+  const initialCount = router?.query?.count
+  const newCount = initialCount && +initialCount
+  const FormsIndices = Array.from({ length: newCount }, (_, index) => index);
 
- /*  const handleSubmit = (e) => {
-    console.log("sdada", e)
-  } */
-  const initialValues = {
-    nombre: "",
-    email: "",
-    verificarEmail: "",
-    telefono: "",
-  }
+  return (
+    <>
+      <Formik
+        initialValues={{
+          nombre:"",
+          email:"",
+          telefono:"",
+        }}
+        onSubmit={(values) => {
+          console.log('Valores enviados:', values);
+        }}
+      >
+        {({ handleSubmit }) => (
+          <Form onSubmit={handleSubmit}>
+            {FormsIndices.map((index) => (
+              <div key={index} className="w-[400px] mb-5 rounded-md bg-white shadow-[0px_1px_5px_rgba(0,_0,_0,_0.12),_0px_2px_2px_rgba(0,_0,_0,_0.14),_0px_3px_1px_-2px_rgba(0,_0,_0,_0.2)] flex flex-col  text-gray-600 pb-5 px-5 pt-2 space-y-4">
+                <div className="w-full flex flex-col items-start justify-start py-[10.5px] pl-[21px]">
+                  <b className=" leading-[21px] uppercase text-gray-600 inline-block">
+                    Datos del Comprador #{index + 1}
+                  </b>
+                </div>
+                <div className="h-[100%)] w-[100%] flex flex-col items-start justify-start">
+                  <InputField
+                    id={`nombre-${index}`}
+                    name={`nombre-${index}`}
+                    label="Nombre y apellidos"
+                    labelClass={false}
+                  />
+                </div>
+                <div className="h-[100%)] w-[100%] flex flex-col items-start justify-start">
+                  <InputField
+                    id={`email-${index}`}
+                    name={`email-${index}`}
+                    label="Correo electrónico"
+                    labelClass={false}
+                  />
+                </div>
+                <div className="h-[100%)] w-[100%] flex flex-col items-start justify-start">
+                  <InputField
+                    id={`telefono-${index}`}
+                    name={`telefono-${index}`}
+                    label="Teléfono"
+                    labelClass={false}
+                  />
+                </div>
+              </div>
+            ))}
+            <button type="submit">Enviar</button>
+          </Form>
+        )}
+      </Formik>
 
-  return (<>
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}  >
+      {/* <Formik initialValues={initialValues} onSubmit={handleSubmit}  >
       <Form className="self-stretch w-full h-auto pb-5 px-5 pt-2 space-y-2 ">
         <>
           <AutoSubmit setValuesForm={setValuesForm} />
@@ -56,11 +102,12 @@ export const DatosComprador: FC<propsDatosComprador> = ({ idx, handleSubmit, set
           </div >
         </>
       </Form>
-    </Formik>
-  </>)
+    </Formik> */}
+    </>
+  )
 }
 
-const AutoSubmit = ({ setValuesForm }) => {
+/* const AutoSubmit = ({ setValuesForm }) => {
   const { resetForm, values } = useFormikContext();
 
   useEffect(() => {
@@ -68,4 +115,4 @@ const AutoSubmit = ({ setValuesForm }) => {
   }, [values])
 
   return null;
-};
+}; */
