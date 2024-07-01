b17067204d550791b0aed6961f3fbb8262ddb2d8
eduardo andres <adch.edch@gmail.com>
2024-07-01 15:23:07 -0400

    changues on inputField

--- components/Forms/InputField.tsx
+++ components/Forms/InputField.tsx
@@ -12,6 +12,7 @@ interface propsInputField extends InputHTMLAttributes<HTMLInputElement> {
   className?: string
   disabled?: boolean
   labelClass?: boolean
+
 }
 interface Flag {
   pre: string
@@ -76,7 +77,6 @@ const InputField: FC<Partial<propsInputField>> = ({ label, className, disabled =
         {props?.type === "telefono" &&
           <>
             {showFlags && <ClickAwayListener onClickAway={() => { setShowFlags(false) }}>
-
               <div className={`bg-white w-full h-44 absolute translate-y-10 z-10 border-[1px] rounded-b-xl flex flex-col ${(props?.type !== "tel" ? true : meta.touched) && meta.error ? "border-rose-300" : "border-gray-200"}`}>
                 <input type="text" autoFocus autoComplete="nope" onChange={(e) => setFilterFlags(e.target.value.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, ''))} className="bg-gray-100 h-6 border-0 focus:border-0 w-full py-2 px-4 focus:ring-0 focus:outline-none transition text-xs text-gray-600" />
                 <ul className="w-full flex-1 cursor-pointer text-gray-900 text-xs *space-y-2 *px-2 py-1 overflow-y-scroll">
@@ -102,19 +102,19 @@ const InputField: FC<Partial<propsInputField>> = ({ label, className, disabled =
             <div onClick={() => {
               setShowFlags(!showFlags)
               setOptions(flags)
-            }} className="absolute w-10 h-10 flex justify-start items-center cursor-pointer">
-              <IoIosArrowDown className="text-gray-500" />
+            }} className="absolute w-12 h-9 flex justify-start items-center cursor-pointer ml-[1.5px] pl-2 bg-slate-100 rounded-l-xl mt-[1px]">
               {optionSelect?.pre && <img src={`flags-svg/${optionSelect?.pre}.svg`.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '')} width={22} className="border-[1px] border-gray-500" />}
+              <IoIosArrowDown className="text-gray-500" />
             </div>
           </>
         }
-        {props?.type !== "tel"
-          ? <input id={props?.type} autoFocus={props?.type === "telefono"} disabled={!isAllowed() || disabled} className={`${props?.type === "telefono" && "pl-11"} font-display text-sm text-gray-500 border-[1px] ${(props?.type !== "tel" ? true : meta.touched) && meta.error ? "border-rose-300" : "border-gray-200"} focus:border-gray-400 w-full py-2 px-4 rounded-xl focus:ring-0 focus:outline-none transition ${className}`} {...field} {...props} type={props?.type === "telefono" ? "tel" : "text"}></input>
+        {props?.type != "tel"
+          ? <input id={props?.type} autoFocus={props?.type === "telefono"} disabled={!isAllowed() || disabled} className={`${props?.type === "telefono" && "pl-14"} font-display text-sm text-gray-500 border-[1px] ${(props?.type !== "tel" ? true : meta.touched) && meta.error ? "border-rose-300" : "border-gray-200"} focus:border-gray-400 w-full py-2 px-4 rounded-xl focus:ring-0 focus:outline-none transition ${className}`} {...field} {...props} type={props?.type === "telefono" ? "tel" : props?.type }/>
           : <div onBlur={() => helpers.setTouched(true)} >
           </div>
         }
       </div>
-      {(props?.type !== "tel" ? true : meta.touched) && meta.error && <p className="font-display absolute rounded-xl text-xs left-0 bottom-0 transform translate-y-full text-red flex gap-1">{meta.error}</p>}
+      {(props?.type != "tel" ? true : meta.touched) && meta.error && <p className="font-display absolute rounded-xl text-xs left-0 bottom-0 transform translate-y-full text-red flex gap-1">{meta.error}</p>}
       <style jsx>
         {`
         input[type=number]::-webkit-inner-spin-button, 
