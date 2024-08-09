fc7e49ae1bc109a0ff51988f983a5640879becdb
eduardo andres <adch.edch@gmail.com>
2024-08-09 11:03:12 -0400

    update fild

--- components/Facturacion/HistorialFacturacion.tsx
+++ components/Facturacion/HistorialFacturacion.tsx
@@ -4,11 +4,29 @@ import { useEffect, useState } from "react";
 import { AuthContextProvider } from "../../context";
 import { fetchApiBodas, queries } from "../../utils/Fetching";
 import { PiNewspaperClippingThin } from "react-icons/pi";
+import { useRouter } from "next/router";
 
 export const HistorialFacturacion = () => {
     const { config } = AuthContextProvider();
     const [show, setShow] = useState({ state: false, idx: null });
     const [dataFactura, setDataFactura] = useState({ results: [], total: 0 })
+    const [activeSpiner, setActiveSpiner] = useState(false)
+    const router = useRouter()
+
+    console.log(dataFactura)
+
+    const options: object = {
+        year: "2-digit",
+        month: "2-digit",
+        day: "2-digit",
+    }
+
+    useEffect(() => {
+        setActiveSpiner(false)
+        setTimeout(() => {
+            setActiveSpiner(true)
+        }, 1000);
+    }, [])
 
     useEffect(() => {
         fetchApiBodas({
@@ -24,13 +42,13 @@ export const HistorialFacturacion = () => {
         <>
             <div className=" w-full pt-6 relative rounded-lg pb-10">
                 <div className="grid grid-cols-12 px-5 justify-between border-b py-4 border-gray-100  transition bg-white capitalize rounded-t-lg">
-                    <div className="items-center col-span-3 flex flex-col ">
-                        <p className="font-body text-[15px] font-semibold text-gray-600">Id. de la Factura</p>
+                    <div className="items-center md:col-span-3 flex flex-col ">
+                        <p className="font-body text-[15px] font-semibold text-gray-600 hidden md:block">Id. de la Factura</p>
                     </div>
                     <div className="items-center col-span-2 flex flex-col h-full">
                         <p className="font-body text-[15px] font-semibold text-gray-600">Fecha</p>
                     </div>
-                    <div className="items-center col-span-3 flex flex-col h-full">
+                    <div className="items-center md:col-span-3 col-span-5 flex flex-col h-full">
                         <p className="font-body text-[15px] font-semibold text-gray-600">Total</p>
                     </div>
                     <div className="items-center col-span-2 flex flex-col h-full">
@@ -40,22 +58,24 @@ export const HistorialFacturacion = () => {
                         <p className="font-body text-[15px] font-semibold text-gray-600"></p>
                     </div>
                 </div>
-                <div className=" bg-white rounded-b-lg max-h-[calc(100vh-320px)] h-[calc(100vh)] overflow-y-auto">
+                <div className="bg-white rounded-b-lg max-h-[calc(100vh-320px)] h-[calc(100vh)] overflow-y-auto overflow-x-auto">
                     {
-                        dataFactura.results.length ?
+                        activeSpiner ?
                             dataFactura?.total > 0 ?
                                 dataFactura?.results?.map((item, idx) => {
                                     return (
-                                        <div key={idx} className={`grid grid-cols-12 px-5 justify-between py-4 transition gap-y-4 text-gray-600 ${idx % 2 === 0 ? "bg-gray-50" : ""}`}>
-                                            <div className="items-center col-span-3 flex flex-col ">
-                                                {item.number}
+                                        <div key={idx} className={`grid grid-cols-12 md:px-5 justify-between py-4 transition gap-y-4 text-gray-600 ${idx % 2 === 0 ? "bg-gray-50" : ""}`}>
+                                            <div className=" items-center md:col-span-3  flex flex-col">
+                                                <div className=" hidden md:block">
+                                                    {item.number}
+                                                </div>
                                             </div>
-                                            <div className="col-span-2 flex flex-col h-full justify-center items-center">
-                                                {item.created}
+                                            <div className=" col-span-2 flex flex-col h-full justify-center items-center">
+                                                {`${new Date(item?.created).toLocaleDateString(undefined, options)}`}
                                             </div>
-                                            <div className="col-span-3 flex h-full justify-center items-center space-x-1">
+                                            <div className="md:col-span-3 col-span-5 flex h-full justify-center items-center space-x-1">
                                                 <span>
-                                                    {item.amount / 100}
+                                                    {(item.amount / 100).toFixed(2)}
                                                 </span>
                                                 <span>
                                                     {item.currency}
@@ -108,7 +128,7 @@ export const HistorialFacturacion = () => {
                                                     </div>
                                                 }
                                             </div>
-                                            <div className="col-span-2 flex flex-col h-full justify-center items-center">
+                                            <div className="col-span-2 flex flex-col h-full justify-center items-center relative">
                                                 <span
                                                     onClick={() => setShow({ state: !show.state, idx: idx })}
                                                     className={`cursor-pointer relative w-max rounded-lg text-sm text-gray-700`}
@@ -118,12 +138,17 @@ export const HistorialFacturacion = () => {
                                                 {
                                                     show.idx === idx &&
                                                     <ClickAwayListener onClickAway={() => show && setShow({ state: false, idx })}>
-                                                        <div className="w-full flex justify-center items-center relative">
-                                                            <ul
-                                                                className={`${show.state === true ? "block" : "hidden"} top-0.5 right-16 absolute w-max border border-base bg-white capitalize rounded-md  shadow-lg z-10 translate-x-[-12px] p-3 cursor-pointer hover:bg-gray-100`}
-                                                            >
-                                                                Descargar Factura
-                                                            </ul>
+                                                        <div
+                                                            className={`${show.state === true ? "block" : "hidden"} right-16 top-5 absolute w-max border bg-white capitalize rounded-md shadow-lg z-10 translate-x-[-12px] cursor-pointer h-max `}
+                                                        >
+                                                            <div className="w-full flex flex-col justify-center items-center">
+                                                                <a href={`${item.hostedInvoiceUrl}`} target="_blank" rel="noreferrer" className="hover:bg-gray-100 py-3 px-2 text-[14px] w-full">
+                                                                    Ver Factura
+                                                                </a>
+                                                                <a href={`${item.invoicePdf}`} target="_blank" rel="noreferrer" className="hover:bg-gray-100 py-3 px-2 text-[14px] w-full">
+                                                                    Descargar Factura
+                                                                </a>
+                                                            </div>
                                                         </div>
                                                     </ClickAwayListener>
                                                 }
