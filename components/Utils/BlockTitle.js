83e98db3b5e613068e508ba0db65e0161c0c703c
eduardo andres <adch.edch@gmail.com>
2024-09-07 17:21:06 -0400

    block cabecera update

--- components/Utils/BlockTitle.js
+++ components/Utils/BlockTitle.js
@@ -6,7 +6,7 @@ import { IoShareSocial } from 'react-icons/io5'
 import { DiGoogleDrive } from "react-icons/di";
 import { Modal } from './Modal'
 
- export const BlockTitle = ({ title }) => {
+export const BlockTitle = ({ title }) => {
 
   const { forCms, user } = AuthContextProvider()
   const { event } = EventContextProvider()
@@ -17,26 +17,26 @@ import { Modal } from './Modal'
     <div className={`w-full h-14 bg-white rounded-xl shadow-lg ${forCms ? "hidden" : "flex"} items-center justify-between`}>
       <ModalAddUserToEvent openModal={openModal} setOpenModal={setOpenModal} event={event} />
       <div className='flex md:flex-1 flex-col px-2 md:px-6 font-display'>
-        <span className='md:hidden capitalize text-gray-600 leading-[20px] font-medium'>{event?.nombre}</span>
-        <span className='md:hidden capitalize text-primary text-[12px] leading-[12px]'>{event?.tipo}</span>
-        <span className="text-gray-500 text-[18px] leading-[20px] font-bold">
-          {title}
-        </span>
+        <span className="text-gray-500 text-[18px] leading-[20px] font-bold">{title}</span>
+        <div className='space-x-1'>
+          <span className='md:hidden capitalize text-primary text-[12px] leading-[12px]'>{event?.tipo}</span>
+          <span className='md:hidden capitalize text-gray-600 text-[12px] leading-[20px] font-medium'>{event?.nombre}</span>
+        </div>
       </div>
       <div className='flex-1 md:flex-none md:w-[35%] h-[100%] flex flex-row-reverse md:flex-row items-center '>
         <img
           src={defaultImagenes[event?.tipo]}
-          className=" h-[90%] object-cover object-top rounded-md border-1 border-gray-600"
+          className=" h-[90%] object-cover object-top rounded-md border-1 border-gray-600  hidden md:block"
           alt={event?.nombre}
         />
         <div className='hidden md:flex flex-col font-display font-semibold text-md text-gray-500 px-2 md:pt-2 gap-2'>
           <span className='text-sm translate-y-2 text-primary text-[12px] first-letter:capitalize'>{event?.tipo}</span>
           <span className='uppercase w-64 truncate '>{event?.nombre}</span>
         </div>
-        <div className='flex '>
+        <div className='flex'>
           <div className='flex items-center justify-center'>
             <div onClick={() => { event?.usuario_id === user?.uid && setOpenModal(!openModal) }}
-              className={`-translate-y-1.5 -translate-x-8 md:-translate-y-[15px] ${title === "Presupuesto" ? "md:-translate-x-14" : "md:-translate-x-8"} `}>
+              className={`-translate-y-[13px] -translate-x-3 md:-translate-y-[15px] ${title === "Presupuesto" ? "md:-translate-x-14" : "md:-translate-x-8"} `}>
               <UsuariosCompartidos event={event} />
             </div>
             <span
@@ -59,7 +59,7 @@ import { Modal } from './Modal'
         openModalDrive ?
           <Modal openIcon={openModalDrive} setOpenIcon={setOpenModalDrive} classe={"h-max w-[40%]"}>
             <div className='my-10 mx-32'>
-              <img src='/WIP.png'  />
+              <img src='/WIP.png' />
             </div>
           </Modal>
           : null
