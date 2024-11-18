aa6dc1cd40ae3030437c1feb34be79d5b4638a1e
eduardo andres <adch.edch@gmail.com>
2024-11-18 11:37:29 -0400

    re deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -329,7 +329,7 @@ const GridCards: FC<propsGridCards> = ({ state, set: setNewEvent }) => {
                           </div>
                         </SwiperSlide> :
                         <SwiperSlide
-                          className={`flex items-center justify-center my-3`}
+                          className={`flex items-center justify-center my-3 `}
                         >
                           <CardEmpty state={state} set={setNewEvent} />
                         </SwiperSlide>
