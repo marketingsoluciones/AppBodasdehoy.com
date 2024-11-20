c125797dd2c9bffd76149943c275e5803db17f55
eduardo andres <adch.edch@gmail.com>
2024-11-20 13:22:13 -0400

    add function to share itinerary vew

--- pages/event/[...slug].tsx
+++ pages/event/[...slug].tsx
@@ -1,7 +1,7 @@
 
 import { FC } from "react";
 import { fetchApiEventos, queries } from "../../utils/Fetching";
-import { Event} from "../../utils/Interfaces";
+import { Event } from "../../utils/Interfaces";
 import { ItinerarioPDF } from "../../components/Itinerario/MicroComponente/ItinerarioPDF";
 import { InvitadosPDF } from "../../components/Invitados/InvitadosPDF";
 
@@ -14,17 +14,71 @@ const Slug: FC<props> = (props) => {
   const p = props?.slug[0]?.split("-")
   const recurse = p[0]
 
-  if(recurse === "itinerary"){
+  if (recurse === "itinerary") {
     return (
       <ItinerarioPDF props={props} />
     )
   }
 
-  if(recurse === "guest"){
-    return (
-      <InvitadosPDF props={p[1]} />
-    )
-  }
+  return (
+    <section className={"absolute z-[50] w-[calc(100vw-40px)] h-[100vh] top-0 left-4 bg-white"}>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="max-w-screen-lg mx-auto inset-x-0 w-full pl-2 pr-[1px] md:px-0 gap-4 relative">
+        <div className={`w-full h-14 rounded-xl shadow-lg flex items-center justify-between`}>
+          <div className='flex md:flex-1 flex-col px-2 md:px-6 font-display'>
+            <div className='space-x-1'>
+              <span className='md:hidden capitalize text-primary text-[12px] leading-[12px]'>{event?.tipo}</span>
+              <span className='md:hidden capitalize text-gray-600 text-[12px] leading-[20px] font-medium'>{event?.nombre}</span>
+            </div>
+          </div>
+          <div className='flex-1 md:flex-none md:w-[35%] h-[100%] flex flex-row-reverse md:flex-row items-center '>
+            <img
+              src={defaultImagenes[event?.tipo]}
+              className=" h-[90%] object-cover object-top rounded-md border-1 border-gray-600  hidden md:block"
+              alt={event?.nombre}
+            />
+            <div className='hidden md:flex flex-col font-display font-semibold text-md text-gray-500 px-2 md:pt-2 gap-2'>
+              <span className='text-sm translate-y-2 text-primary text-[12px] first-letter:capitalize'>{event?.tipo}</span>
+              <span className='uppercase w-64 truncate '>{event?.nombre}</span>
+            </div>
+          </div>
+        </div>
+        <div className="w-full px-4 md:px-10 py-4" >
+          <div className="flex flex-col justify-center items-center">
+            <span className="text-3xl md:text-[40px] font-title text-primary">{event?.itinerarios_array[0]?.title}</span>
+            <div className="w-[100px] bg-primary h-0.5 rounded-md mt-2" />
+          </div>
+        </div >
+        {tasksReduce?.map((el, i) =>
+          <div key={i} className="w-full *h-[500px] mt-4">
+            <div className={`w-full flex justify-start`}>
+              <span className={`border-primary border-dotted mb-1 border-[1px] px-5 py-[1px] rounded-full text-[12px] font-semibold`}>
+                {new Date(el?.fecha).toLocaleString(geoInfo?.acceptLanguage?.split(",")[0], { year: "numeric", month: "long", day: "2-digit" })}
+              </span>
+            </div>
+            {el?.tasks?.map((elem, idx) => {
+              return (
+                <TaskNew
+                  key={idx}
+                  task={elem}
+                  itinerario={event.itinerarios_array[0]}
+                  view={"schema"}
+                  // isSelect={selectTask === elem._id}
+                  onClick={() => { }}
+                />
+              )
+            })}
+          </div>
+        )
+        }
+        {end && <span id="elementControl" className="text-xs">~</span>}
+      </motion.div>
+    </section>
+  )
+
 };
 
 export default Slug;
