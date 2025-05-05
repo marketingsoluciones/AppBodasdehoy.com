6f6c4367877e784be91100e73677b20135acf247
eduardo andres <adch.edch@gmail.com>
2025-05-05 15:09:29 -0400

    update metadata

--- pages/servicios.tsx
+++ pages/servicios.tsx
@@ -77,10 +77,7 @@ export async function getServerSideProps({ params, query }) {
         }) as any
         if (evento) {
             openGraphData.openGraph.title = `${evento.itinerarios_array[0].tasks[0].descripcion}`
-            openGraphData.openGraph.description = `
-            Evento = ${evento.nombre} 
-            Tipo = ${evento.tipo}
-            Fecha = ${new Date(parseInt(evento?.itinerarios_array[0].fecha_creacion)).toLocaleDateString("es-VE", { year: "numeric", month: "long", day: "numeric", timeZone: "UTC" })}
+            openGraphData.openGraph.description = ` El Evento ${evento.tipo}, ${evento.nombre}, ${new Date(parseInt(evento?.itinerarios_array[0].fecha_creacion)).toLocaleDateString("es-VE", { year: "numeric", month: "long", day: "numeric", timeZone: "UTC" })}
             ${evento.itinerarios_array[0].tasks[0].tips.replace(/<[^>]*>/g, "").replace(".", ". ")}`
         }
         return {
