48210c3d1d9c1c39c7b0f7769d19304a02004ff4
eduardo andres <adch.edch@gmail.com>
2025-05-20 10:50:44 -0400

    add email editor

--- components/Invitaciones/Banner.js
+++ components/Invitaciones/Banner.js
@@ -2,10 +2,12 @@ import { useContext } from 'react'
 import EventoContext from '../../context/EventContext'
 import { EventContextProvider } from "../../context";
 import { useTranslation } from 'react-i18next';
+import { useRouter } from 'next/router';
 
 const Banner = () => {
   const { t } = useTranslation();
   const { event } = EventContextProvider()
+  const router = useRouter()
 
   const banners = {
     boda: "boda.webp",
@@ -33,7 +35,7 @@ const Banner = () => {
           <div className="w-max h-max relative">
             <p className="font-display text-xs text-white">{t("foronly")}</p>
             <p className="font-display text-sm text-white">{t("yourinvitation")}</p>
-            <button className="focus:outline-none bg-tertiary text-gray-500 font-display text-sm px-2 rounded-lg w-full hover:text-primary hover:bg-white transition">{t("todesign")}</button>
+            <button onClick={() => router.push('/InvitationEmailEditor') } className="focus:outline-none bg-tertiary text-gray-500 font-display text-sm px-2 rounded-lg w-full hover:text-primary hover:bg-white transition">{t("todesign")}</button>
             <div className="bg-tertiary w-20 h-20 absolute rounded-full top-0 right-0 translate-x-1/2 -translate-y-3/4 transform flex items-center justify-center">
               <p className="font-display text-primary text-5xl font-bold">5<span className="text-xl font-light">€</span></p>
             </div>
