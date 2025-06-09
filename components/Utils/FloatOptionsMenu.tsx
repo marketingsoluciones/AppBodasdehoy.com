192dd76679391bc3ecc43db0d488cb889881dc02
eduardo andres <adch.edch@gmail.com>
2025-06-09 12:58:48 -0400

    position fixed on menu flotante into table budget

--- components/Utils/FloatOptionsMenu.tsx
+++ components/Utils/FloatOptionsMenu.tsx
@@ -2,6 +2,8 @@ import { FC, useEffect, useState } from "react"
 import { FloatMenu } from "../../utils/Interfaces"
 import ClickAwayListener from "react-click-away-listener"
 
+
+
 interface props {
   showOptionsMenu: { state: boolean, values?: FloatMenu, isLoading?: boolean, select?: string }
   setShowOptionsMenu: any
@@ -15,13 +17,13 @@ export const FloatOptionsMenu: FC<props> = ({ showOptionsMenu, setShowOptionsMen
       <div className="select-none">
         <div
           style={position ? { left: position.x, top: position.y } : {}}
-          className={`bg-gray-50 absolute z-30 rounded-lg border-[1px] border-gray-200 shadow-md overflow-hidden 
+          className={`bg-gray-50 absolute z-30 rounded-lg border-[1px] border-gray-200 position shadow-md overflow-hidden 
             ${!position
-              ? `${justify === "end" ? "translate-x-8" : "-translate-x-[calc(100%)]"} ${aling === "botton" ? "-translate-y-[calc(100%+4px)]" : "translate-y-5"}`
+              ? `${justify === "end" ? "translate-x-8" : "-translate-x-[calc(100%)]. "} ${aling === "botton" ? "-translate-y-[calc(100%+4px)]" : "translate-y-5"}`
               : `${justify === "end" ? "-translate-x-full" : "translate-x-6"} ${aling === "botton" ? "-translate-y-1/2" : "translate-y-1/2"}`} `}
         >
-          {options.filter((elem)=>{
-            if(elem?.object){
+          {options.filter((elem) => {
+            if (elem?.object) {
               return elem.object.includes(info?.row?.original?.object);
             }
             return elem?.object === info?.row?.original?.object
@@ -37,6 +39,25 @@ export const FloatOptionsMenu: FC<props> = ({ showOptionsMenu, setShowOptionsMen
             </div>
           )}
         </div>
+        <style jsx>
+          {`
+          .position {
+            right: 100px;
+          }
+
+          @media only screen and (min-width: 1536px) {
+            .position {
+              right: 220px;
+            }
+          }
+          @media only screen and (min-width: 1280px) {
+            .position {
+              right: 60px;
+            }
+          }
+        `}
+        </style>
+
       </div>
     </ClickAwayListener>
   )
