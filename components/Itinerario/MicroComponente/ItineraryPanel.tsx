b67d2e439b25a30e4d6108477067b7b7787d1514
ChristianWork05 <christian.lanza@recargaexpress.com>
2025-06-15 05:11:45 -0400

    refactor: ajustar el estilo de visualización de tareas en ItineraryPanel

--- components/Itinerario/MicroComponente/ItineraryPanel.tsx
+++ components/Itinerario/MicroComponente/ItineraryPanel.tsx
@@ -571,7 +571,7 @@ const handleTaskCreate = async (taskData: Partial<Task>) => {
                         />
                       ) : view !== "table"
                             ? tasksReduce?.map((el, i) =>
-                                <div key={i} className="w-full mt-4">
+                                <div key={i} className="w-full mt-4 flex flex-col gap-4">
                                     {["/itinerario"].includes(window?.location?.pathname) && <div className={`w-full flex ${view === "schema" ? "justify-start" : "justify-center"}`}>
                                         <span className={`${view === "schema" ? "border-primary border-dotted mb-1" : "border-gray-300 mb-1"} border-[1px] px-5 py-[1px] rounded-full text-[12px] font-semibold`}>
                                             {new Date(el?.fecha).toLocaleString(geoInfo?.acceptLanguage?.split(",")[0], { year: "numeric", month: "long", day: "2-digit" })}
