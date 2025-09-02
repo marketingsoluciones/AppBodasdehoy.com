31c806887964110f7cdaa46ee519534568279e94
eduardo andres <adch.edch@gmail.com>
2025-09-02 11:25:45 -0400

    escondiendo funcion

--- pages/invitados.tsx
+++ pages/invitados.tsx
@@ -108,7 +108,7 @@ const Invitados: FC = () => {
               className="max-w-screen-lg mx-auto inset-x-0 w-full px-2 md:px-0 gap-4 relative"
             >
               <BlockCabecera />
-              <div className="absolute z-10  right-5 md:right-[155px] translate-y-3 md:top-[170px]">
+              <div className="absolute z-10  right-5 md:right-[155px] translate-y-3 md:top-[170px] md:hidden">
                 <SelectModeView value={viewPreferUser} setValue={setViewPreferUser} />
               </div>
               {view === "cards"
